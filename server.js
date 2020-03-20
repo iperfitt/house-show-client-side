fc274d188765c05297ae20752798d2c2f1906cd0
Ian Perfitt <ianperfitt@gmail.com>
2020-03-20 12:57:10 -0500

    added route for /

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
-const express = require('express');
+import express from "express";
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
