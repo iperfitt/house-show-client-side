3e61ed1fab9fd56eea948eb96bda6217a0f7320f
Ian Perfitt <ianperfitt@gmail.com>
2020-03-27 16:18:05 -0500

    removed static folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 var path = require('path');
 // Run the app by serving the static files
 // in the dist directory
- app.use(express.static('src/app/dist'));
+ app.use(express.static(__dirname + '/src/app/dist'));
 //app.use(express.static(${__dirname}/dist/'));
 // Start the app by listening on the default
 // Heroku port
@@ -13,7 +13,7 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile( 'src/dist/index.html');
+  res.sendFile('src/app/dist/index.html', { root: __dirname } );
 });
 // app.get('*', (req, res) => {
 //   res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
