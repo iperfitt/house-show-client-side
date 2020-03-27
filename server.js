6680a4e021a48044826e4906916b2e61f6fa4130
Ian Perfitt <ianperfitt@gmail.com>
2020-03-27 14:44:29 -0500

    updated angular.json outputPath from /app/dist to src/app/dist

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // server.js
-import express from 'express';
+var express = require('express');
 const app = express();
-import join from 'path';
+var path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -12,22 +12,6 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// // server.js
-// var express = require('express');
-// const app = express();
-// var path = require('path');
-// // Run the app by serving the static files
-// // in the dist directory
-// app.use(express.static(__dirname + '/dist'));
-// // Start the app by listening on the default
-// // Heroku port
-// app.listen(process.env.PORT || 8080);
-// // ...
-// // For all GET requests, send back index.html
-// // so that PathLocationStrategy can be used
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
