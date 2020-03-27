5efecaa72f19cd2cf95cfa13250a154ab6c93e5f
Ian Perfitt <ianperfitt@gmail.com>
2020-03-27 17:46:37 -0500

    updated server.js

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.listen(process.env.PORT || 8080);
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('*', function(req, res) {
-  res.sendFile('src/app/dist/index.html', { root: __dirname } );
-});
-// app.get('*', (req, res) => {
-//   res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// app.get('*', function(req, res) {
+//   res.sendFile('src/app/dist/index.html', { root: __dirname } );
 // });
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'src/app/dist', 'index.html'));
+});
+
