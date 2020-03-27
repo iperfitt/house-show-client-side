06dadb0db7f24ad2b64f42a289f8c66160ca8e5d
Ian Perfitt <ianperfitt@gmail.com>
2020-03-27 14:55:51 -0500

    updated angular.json

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-// server.js
+// create an express application
 var express = require('express');
 const app = express();
 var path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+app.use(express.static('${__dirname}/dist/'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+app.get('*', (req, res) => {
+  res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-
