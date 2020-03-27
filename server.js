98a2e72eb74c6c9a9de0e5667e92695abf83ef6e
Ian Perfitt <ianperfitt@gmail.com>
2020-03-27 16:48:58 -0500

    updated server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 8080);
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile('src/app/dist/index.html', { root: __dirname } );
 });
 // app.get('*', (req, res) => {
