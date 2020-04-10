49fcc18145e2fcaf1c46f19b38be5aced91a11da
Ian Perfitt <ianperfitt@gmail.com>
2020-04-10 18:33:51 -0500

    created eventType,Name,and genre filtering functionalities

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -13,25 +13,63 @@ export class EventListComponent implements OnInit {
   constructor(private eventService: EventService, private router: Router, private stateService: StateService) { }
 
   events : any[]
+
   filteredEvents : any[]
 
-  private _listFilter: string;
+  private _cityFilter: string;
 
-  public get listFilter(): string {
-    return this._listFilter;
-  }
+  private _typeFilter: string;
+
+  private _nameFilter: string;
+
+  private _genreFilter: string;
+
+  // public get listFilter(): string {
+  //   return this._cityFilter;
+  // }
   
-  public set listFilter(value: string) {
-      this._listFilter = value;
-      this.filteredEvents = this._listFilter ? this.performFilter(this._listFilter) : this.events;
+  public set nameFilter(value: string) {
+      this._nameFilter = value;
+      this.filteredEvents = this._nameFilter ? this.filterName(this._nameFilter) : this.events;
   }
 
-  performFilter(filterBy: string): any[] {
+  public set genreFilter(value: string) {
+    this._genreFilter = value;
+    this.filteredEvents = this._genreFilter ? this.filterGenre(this._genreFilter) : this.events;
+}
+
+public set cityFilter(value: string) {
+  this._cityFilter = value;
+  this.filteredEvents = this._cityFilter ? this.filterCity(this._cityFilter) : this.events;
+}
+
+public set typeFilter(value: string) {
+this._typeFilter = value;
+this.filteredEvents = this._typeFilter ? this.filterType(this._typeFilter) : this.events;
+}
+
+  filterCity(filterBy: string): any[] {
     filterBy = filterBy.toLocaleLowerCase();
-    console.log(this.events.filter((event: any) =>
-          event.eventAddress.city.toLowerCase().indexOf(filterBy) !== -1));
-    return this.events.filter((event: any) =>
-    event.eventAddress.city.toLowerCase().indexOf(filterBy) !== -1)
+    return (this.events.filter((event: any) =>
+    event.eventAddress.city.toLowerCase().indexOf(filterBy) !== -1));
+}
+
+filterType(filterBy: string): any[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return (this.events.filter((event: any) =>
+  event.eventType.toLowerCase().indexOf(filterBy) !== -1));
+}
+
+filterName(filterBy: string): any[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return (this.events.filter((event: any) =>
+  event.eventName.toLowerCase().indexOf(filterBy) !== -1));
+}
+
+filterGenre(filterBy: string): any[] {
+filterBy = filterBy.toLocaleLowerCase();
+return (this.events.filter((event: any) =>
+event.eventGenre.toLowerCase().indexOf(filterBy) !== -1));
 }
   
   ngOnInit() {
