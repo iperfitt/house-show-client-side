d2c9681f3be7882983e6508993528333420c5324
Ian Perfitt <ianperfitt@gmail.com>
2020-04-10 18:42:33 -0500

    deleted all pipes

--- src/app/pipes/cityfilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CityfilterPipe } from './cityfilter.pipe';
-
-describe('CityfilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CityfilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
